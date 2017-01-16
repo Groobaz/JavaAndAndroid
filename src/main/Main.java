bdc35ec10dce8bcbfc13c24cb86ff22926b889f0
Michal R <jakisemail@gmail.com>
2017-01-16 19:38:53 +0100

    Dodalem Profukt w lodówce

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import utils.ProductInAFridge;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Jestem Ksiazka kucharska");
+
+		ProductInAFridge product1 = new ProductInAFridge("ziemniaki", "wegetarianski", 5, "szt.");
+		System.out.println(product1.toString());
 	}
 
 }
---
+++ src/utils/ProductInAFridge.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class ProductInAFridge extends AbstractProduct {
+
+	public ProductInAFridge(String name, String type, Integer quantity, String quantityType) {
+		super(name, type, quantity, quantityType);
+
+		}
+
+}
