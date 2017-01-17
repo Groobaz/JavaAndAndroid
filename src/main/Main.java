2c7c32c34981285a6f6d953b41248b17236c5856
Kamil <w-kamil@wp.pl>
2017-01-17 18:07:01 +0100

    Testowe zmiany na zajeciach

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,8 +12,9 @@ public class Main {
 		System.out.println(product1.toString());
 		
 		ProductInARecipe product2 = new ProductInARecipe("ziemniaki", "wegetarianski", 5, "szt.");
-		System.out.println(product2.toString() + "utworzenie  go produktu1");
+	
 		
+		System.out.println("Nowy komentarz");
 	}
 
 }
